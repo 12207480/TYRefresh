c2bdcd03d02b57b8ce68863b57ddf5ef2d4bcb04
tanyang <122074809@qq.com>
2016-10-08 15:40:59 +0800

    Initial Commit

---
+++ TYRefreshDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYRefreshDemo
+//
+//  Created by tany on 16/10/8.
+//  Copyright © 2016年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
